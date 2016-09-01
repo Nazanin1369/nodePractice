5ff80f6c03a037975f53956ad1b51f3f5ce4e83d
nazanin1369 <nazanin1427@gmail.com>
2016-08-31 19:56:50 -0700

    :tada: chatServer

--- chatServer/tcpServer.js
+++ chatServer/tcpServer.js
@@ -1,14 +1,31 @@
 var net = require('net');
 
-var chatServer = net.createServer();
+var chatServer = net.createServer(),
+    clients = [];
 
-chatServer.on('connection', function(client) {
-    client.write('hi\n');
-    client.write('bye\n');
+chatServer.on('connection', function(socket) {
 
-    client.end();
-});
+    socket.name = socket.remoteAddress + " - " + socket.remotePort;
 
-chatServer.listen(9000);
+    clients.push(socket);
 
-console.log('TCP server started');
+    socket.on('data', function(data) {
+        console.log(socket.name + ' > ' +  data);
+    });
+
+    socket.on('end', function() {
+        clients.splice(clients.indexOf(socket), 1);
+        broadcast(socket.name + " left the chat.\n");
+    });
+
+    function broadcast(message, sender) {
+        clients.forEach(function(client) {
+            if (client === sender) return;
+            client.write(message);
+        });
+        console.log(message);
+    }
+}).listen(9000);
+
+
+console.log('TCP server started on port 9000');
